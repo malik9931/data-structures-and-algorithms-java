b8e1a43929f0333f784c377205fe3c04cc560861
Malek <engmalek.civil@gmail.com>
2021-04-27 01:23:41 +0300

    lab 15 is DONE

--- Data-Structures2/app/src/main/java/tree/treeApp.java
+++ Data-Structures2/app/src/main/java/tree/treeApp.java
@@ -1,7 +1,7 @@
 package tree;
 
 public class treeApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         BinaryTree newTree = new BinaryTree();
 //        System.out.println(newTree.toString());
 
@@ -9,11 +9,29 @@ public class treeApp {
         BinaryTree scndTree = new BinaryTree(one);
         Node two = new Node(2);
         Node three = new Node(3);
+        System.out.println("2nd Tree: "+scndTree.toString());
         one.left = two;
         one.right = three;
 //        System.out.println(one);
         System.out.println(newTree.preOrder(one));
-        System.out.println("2nd Tree: "+scndTree);
+        System.out.println(newTree.inOrder(one));
+        System.out.println(newTree.postOrder(one));
+
+
+        //Creating Binary Search Tree
+//        Node firstRoot = new Node(23);
+//        newBST.root = firstRoot;
+        try {
+        BinarySearchTree newBST = new BinarySearchTree();
+        newBST.add(4);
+        newBST.add(2);
+            System.out.println(newBST.contains(5));
+//        System.out.println(newBST.toString());
+
+        }catch (NullPointerException ex){
+            throw ex;
+        }
+
 
     }
 }
