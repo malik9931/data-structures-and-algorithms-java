c9d5d45709ac21272041351331d72470cf2d484d
Malek <engmalek.civil@gmail.com>
2021-05-24 01:20:03 +0300

    quick-sort is DONE

---
+++ Data-Structures2/app/src/main/java/hashtable/Entry.java
@@ -0,0 +1,25 @@
+//package hashtable;
+//
+//public class Entry<T> {
+//    int key;
+//    T value;
+//    Entry next;
+//
+//    public Entry(){
+//        next= null;
+//    }
+//
+//    public Entry(int key, T value) {
+//        this.key = key;
+//        this.value = value;
+//        next = null;
+//    }
+//
+//    public int getKey() {
+//        return key;
+//    }
+//
+//    public T getValue() {
+//        return value;
+//    }
+//}
